b5c9eff2df47d873ec678d14683de61b48a5c03b
meisterfischy <grbmn@kstn.in>
2022-01-15 18:55:37 +0100

    final touches

--- install.sh
+++ install.sh
@@ -9,21 +9,18 @@ if [ "$#" -ne 1 ]; then
     exit 2
 fi
 
-# Generates the config files from the templates 
 echo "Generating config files from templates"
 for template in `find $FILES -name "*.j2"`; do
     cfg=$(echo $template | sed 's/.j2/_gen/')
     python ./fill_template.py $template $1 > $cfg
 done
-echo "done"
 
-
-# Creates the folders
 echo "Creating fodlers"
 for folder in `find $FILES -mindepth 1 -type d`; do
     mkdir -p $(echo $folder | sed 's/files/'$home'/')
 done
 
+echo "Creating syslinks to config files"
 for file in `find $FILES -mindepth 1 -type f -not -name "*.j2"`; do
     dest=$(echo $file | sed 's/files/'$home'/')
     if [[ $file == *_gen ]]; then
